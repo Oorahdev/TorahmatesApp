5441a8ae3f2f959a1f775b537b54195af9d27f4e
twender <Tziporah.Wender@oorah.org>
2017-11-10 10:48:49 -0500

    MalformedJsonException

---
+++ app/src/main/java/com/vb/torahmate/events/UpdateTokenEvent.java
@@ -0,0 +1,8 @@
+package com.vb.torahmate.events;
+
+/**
+ * Created by twender on 11/9/2017.
+ */
+
+public class UpdateTokenEvent {
+}
---
+++ app/src/main/java/com/vb/torahmate/models/UpdateTokenModel.java
@@ -0,0 +1,8 @@
+package com.vb.torahmate.models;
+
+/**
+ * Created by twender on 11/9/2017.
+ */
+
+public class UpdateTokenModel {
+}
