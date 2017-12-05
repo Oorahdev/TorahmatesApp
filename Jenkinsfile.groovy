855e49dd9e2d9d500e98ade3ab3b450e8cfb5d58
twender <Tziporah.Wender@oorah.org>
2017-12-05 12:47:45 -0500

    added Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+pipeline {
+
+    agent any
+	
+	stages {
+		stage('Build') {
+		    steps{
+                //pull tmapp and server from github
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false,
+                    extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'feac2ef8-3a88-4107-945e-4f359bf1a984',
+                    url: 'https://github.com/Oorahdev/TorahMatesApp-FCM_XMPP_Server'],
+                    [credentialsId: 'feac2ef8-3a88-4107-945e-4f359bf1a984', url: 'https://github.com/Oorahdev/TorahmatesApp']]])
+                //build tmapp apk from github
+
+               }
+			
+		}
+
+
+		stage('Deploy') {
+
+		    steps{
+                //sign android apk
+                sh 'ls'
+                step([$class: 'SignApksBuilder', apksToSign: '**/*.apk', archiveUnsignedApks: true,
+                        keyAlias: 'tmappkey', keyStoreId: 'tmappkey'])
+                //upload app to google play
+                androidApkUpload apkFilesPattern: '/app/app-release.apk', googleCredentialsId: 'Google Play Credentials',
+                    trackName: 'beta'
+                //run server on aws
+               }
+		}
+		
+	}
+}
